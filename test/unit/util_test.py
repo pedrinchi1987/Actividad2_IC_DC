187d65340d72863e01c7b299b02bd75005702797
pedrinchi1987 <pedrito1348@gmail.com>
2022-11-11 00:56:17 -0500

    version1

---
+++ test/unit/util_test.py
@@ -0,0 +1,41 @@
+import unittest
+import pytest
+
+from app import util
+
+
+@pytest.mark.unit
+class TestUtil(unittest.TestCase):
+    def test_convert_to_number_correct_param(self):
+        self.assertEqual(4, util.convert_to_number("4"))
+        self.assertEqual(0, util.convert_to_number("0"))
+        self.assertEqual(0, util.convert_to_number("-0"))
+        self.assertEqual(-1, util.convert_to_number("-1"))
+        self.assertAlmostEqual(4.0, util.convert_to_number("4.0"), delta=0.0000001)
+        self.assertAlmostEqual(0.0, util.convert_to_number("0.0"), delta=0.0000001)
+        self.assertAlmostEqual(0.0, util.convert_to_number("-0.0"), delta=0.0000001)
+        self.assertAlmostEqual(-1.0, util.convert_to_number("-1.0"), delta=0.0000001)
+
+    def test_convert_to_number_invalid_type(self):
+        self.assertRaises(TypeError, util.convert_to_number, "")
+        self.assertRaises(TypeError, util.convert_to_number, "3.h")
+        self.assertRaises(TypeError, util.convert_to_number, "s")
+        self.assertRaises(TypeError, util.convert_to_number, None)
+        self.assertRaises(TypeError, util.convert_to_number, object())
+
+    def test_validate_permissions(self):
+        self.assertEqual(True, util.validate_permissions("1+1", "user1"))
+        self.assertEqual(True, util.validate_permissions("1-1", "user1"))
+        self.assertEqual(False, util.validate_permissions("1-1", "user2"))
+        self.assertEqual(False, util.validate_permissions("1-1", "user2"))
+        
+    def test_InvalidConvertToNumber(self):
+        self.assertEqual(1, util.InvalidConvertToNumber("1"))
+        self.assertEqual(1.5, util.InvalidConvertToNumber("1.5"))
+
+    def test_InvalidConvertToNumber(self):
+        self.assertRaises(TypeError, util.InvalidConvertToNumber, ".")
+        self.assertRaises(TypeError, util.InvalidConvertToNumber, "")        
+        self.assertRaises(TypeError, util.InvalidConvertToNumber, "A")
+        self.assertRaises(TypeError, util.InvalidConvertToNumber, None)
+        self.assertRaises(TypeError, util.InvalidConvertToNumber, object())
